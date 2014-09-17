d01dcc1681822bc6d52acf7b213f3e4486be3e42
Shaun Parsloe <shaunparsloe@hotmail.com>
2014-09-17 19:26:14 +0100

    Messages only respond to their receiveprefix

--- Message.lua
+++ Message.lua
@@ -10,7 +10,7 @@ function Message.new (options)
   self.Text = "";
   self.From = "";
   self.To = "";
-  self.Prefix = "PvPClient";
+  self.ReceivePrefix = "blankprefix";
   self.Header = "";
   self.Body = ""
   self.Time = ""
@@ -32,33 +32,6 @@ function Message:Clone(message)
   return objMsg;
 end
 
-
----- prefix - String - Message prefix, can be used as your addon identifier. (16 Character Limit)
----- text - String - Text to send.
----- type - String - AddOn channel to send to. Valid: "PARTY", "RAID", "GUILD", "OFFICER", "BATTLEGROUND", "WHISPER"
----- target - String - Used only for "WHISPER" communication - the player to whisper to.
---function Message:SendMessage(strType, strMessage, strTarget)
---	if not strMessage then
---		strMessage = "";
---	end
---	print("Preparing message for sending ");
---  self.Header = strType
---  self.Body = self.Header.."|"..strMessage
---  self.Payload = strMessage
---  self.To = strTarget
---  if (strTarget) then
---      print("Message.SendMessage:"..strType..": "..strMessage.." : "..strTarget)
---    SendAddonMessage(self.Prefix, self.Body, "WHISPER", self.To)
---  else
---    --If we are in a party or a raid
---      print("Message.SendMessage:"..strType..": "..strMessage.." : TO PARTY")
---	SendAddonMessage(self.Prefix, self.Body, "PARTY")
---  end
---  self.Time = time()
---
---  return self
---end
---
 function Message:SendMessagePrefixed(strPrefix, strType, strMessage, strTarget)
 	if not strMessage then
 		strMessage = "";
@@ -84,7 +57,7 @@ end
 
 
 function Message:Format(strPrefix, strMessage, strType, strSender)
-  --if strPrefix == self.Prefix then
+  if strPrefix == self.ReceivePrefix then
 	local messageSplit = string_split(strMessage, "|");
 
     self.Text = strMessage;
@@ -98,7 +71,7 @@ function Message:Format(strPrefix, strMessage, strType, strSender)
     self.Type = strType
     self.From = strSender
     self.Time = time()
---  else
---		print("prefix is "..strPrefix.." expecting ".. self.Prefix)
---  end
+  else
+		print("Message prefix is "..strPrefix.." expecting to receive ".. self.ReceivePrefix)
+  end
 end
